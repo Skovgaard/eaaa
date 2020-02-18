80113c426a82a0d2a98e6645deb4bb70d952043c
Kasper Skovgaard <skovgaardkasper@gmail.com>
2020-02-19 00:08:43 +0100

    Ex2 extended

--- Programmering 2/L07 Test/src/ex2/ex2.java
+++ Programmering 2/L07 Test/src/ex2/ex2.java
@@ -2,8 +2,12 @@ package ex2;
 
 public class Ex2 {
 
-    private static final int SMALLPRICE = 2000;
-    private static final int BIGPRICE = 1000;
+    private static final int SMALL_PRICE = 2000;
+    private static final int BIG_PRICE = 1000;
+
+    //For use in totalPaymentCompact
+    private static final int SP = 2000;
+    private static final int BP = 1000;
 
     /*
      * Returns the total payment for a family minus discount
@@ -14,58 +18,52 @@ public class Ex2 {
         if (small + big > 3) {
             int sum = 0;
             if (small > 0 && big < 3) {
-                sum += SMALLPRICE;
+                sum += SMALL_PRICE;
                 small--;
             } else {
-                sum += BIGPRICE;
+                sum += BIG_PRICE;
                 big--;
             }
             for (int i = 0; i < 2; i++) {
                 if (big > 0) {
-                    sum += BIGPRICE * 0.75;
+                    sum += BIG_PRICE * 0.75;
                     big--;
                 } else {
-                    sum += SMALLPRICE * 0.75;
+                    sum += SMALL_PRICE * 0.75;
                     small--;
                 }
             }
-            return (int) (sum + (big * BIGPRICE * 0.5) + (small * SMALLPRICE * 0.5));
+            return (int) (sum + (big * BIG_PRICE * 0.5) + (small * SMALL_PRICE * 0.5));
         } else if (small + big > 1) {
             int sum = 0;
             if (small > 0) {
-                sum += SMALLPRICE;
+                sum += SMALL_PRICE;
                 small--;
             } else {
-                sum += BIGPRICE;
+                sum += BIG_PRICE;
                 big--;
             }
-            return (int) (sum + (big * BIGPRICE * 0.75) + (small * SMALLPRICE * 0.75));
+            return (int) (sum + (big * BIG_PRICE * 0.75) + (small * SMALL_PRICE * 0.75));
         } else if (small + big == 1) {
-            return small > big ? SMALLPRICE : BIGPRICE;
+            return small > big ? SMALL_PRICE : BIG_PRICE;
         } else
             return 0;
     }
-    /*
-    public int totalPayment(int small, int big) {
-        int smallPrice = 2000, bigPrice = 1000;
-        int totalPrice = 0, totalChildren = 0;
-        if (small-- >= 1) {
-            totalPrice += smallPrice;
-            totalChildren++;
-        }
-        while (true) {
-            float discountMod = 1f;
-            if (totalChildren >= 3) discountMod = 0.5f;
-            else if (totalChildren >= 1) discountMod = 0.75f;
-            if (big-- > 0) {
-                totalPrice += (int) (bigPrice * discountMod);
-            } else if (small-- > 0) {
-                totalPrice += (int) (smallPrice * discountMod);
-            } else break;
-            totalChildren++;
-        }
-        return totalPrice;
-    }
-     */
 
+    //Compact
+    public int totalPaymentCompact(int s, int b) {
+        if (s + b <= 1) return s + b == 1 ? s > b ? SP : BP : 0;
+        else if (s + b < 4)
+            return (int) (b > 0 ? (BP * 0.75) + totalPaymentCompact(s, --b) : (SP * 0.75) + totalPaymentCompact(--s, b));
+        else return (int) (s > 0 ? (SP * 0.5) + totalPaymentCompact(--s, b) : (BP * 0.5) + totalPaymentCompact(s, --b));
+    }
+
+    /*
+    // @formatter:off
+    //Minifier
+    public int totalPaymentMinified(int s, int b) {
+        if(s+b<=1)return s+b==1?s>b?SP:BP:0;else if(s+b<4)return(int)(b>0?(BP*0.75)+ totalPaymentCompact(s,--b):(SP*0.75)+ totalPaymentCompact(--s,b));else return(int)(s>0?(SP*0.5)+ totalPaymentCompact(--s,b):(BP*0.5)+ totalPaymentCompact(s,--b));
+    }
+    // @formatter:on
+     */
 }
