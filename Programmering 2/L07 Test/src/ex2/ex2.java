ec7fef9ee3c35b734ca7fdc28ff1a952fc8d9738
Kasper Skovgaard <skovgaardkasper@gmail.com>
2020-02-18 20:11:54 +0100

    Added auth-example

---
+++ Programmering 2/L07 Test/src/ex2/ex2.java
@@ -0,0 +1,14 @@
+package ex2;
+
+public class ex2 {
+
+    /*
+     * Returns the total payment for a family minus discount
+     * Pre: small >= 0, big >= 0
+     * (small is the number of pre-school children
+     * and big is the number of children attending school) */
+    public int totalPayment(int small, int big) {
+
+    }
+
+}
